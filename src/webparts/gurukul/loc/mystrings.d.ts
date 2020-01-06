b16399defd614f8c8e7ede75f474a4be0df5e6dc
Bhavya Bharti <bhavy@ramank-home.redmond.corp.microsoft.com>
2020-01-05 23:10:27 -0800

    scafolding app

---
+++ src/webparts/gurukul/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IGurukulWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'GurukulWebPartStrings' {
+  const strings: IGurukulWebPartStrings;
+  export = strings;
+}
